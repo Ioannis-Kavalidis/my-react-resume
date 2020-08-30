a1d432d127c38ac12eee26a4de5531443ace98dd
paperjar <dreamtnem@gmail.com>
2020-08-30 18:18:29 +0200

    updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,13 @@
 import React from "react";
+import { Container } from "reactstrap";
+import { SectionDivider } from "./components/common";
+import Navigation from "./components/Navigation";
+import AboutSection from "./components/Sections/AboutSection";
+import AwardsSection from "./components/Sections/AwardsSection";
+import EducationSection from "./components/Sections/EducationSection";
+import ExperienceSection from "./components/Sections/ExperienceSection";
+import InterestsSection from "./components/Sections/InterestsSection";
+import SkillsSection from "./components/Sections/SkillsSection";
 
 const App = () => (
   <div className="app">
