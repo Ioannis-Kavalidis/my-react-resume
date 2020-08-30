2eac2942e629083d03c9bdee2462caadaeee39e0
paperjar <dreamtnem@gmail.com>
2020-08-30 18:09:33 +0200

    create navigation, sections, social icons and list of technologies as files with plain jsx files

---
+++ src/components/Sections/SkillsSection/SkillsListItem.jsx
@@ -0,0 +1,19 @@
+import React, { Fragment } from 'react';
+import { string } from 'prop-types';
+import { faCheck } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+const SkillsListItem = ({ item }) => (
+    <Fragment>
+        <li>
+            <FontAwesomeIcon icon={faCheck} />
+            {item}
+        </li>
+    </Fragment>
+);
+
+SkillsListItem.propTypes = {
+    item: string.isRequired,
+};
+
+export default SkillsListItem;
---
+++ src/components/Sections/SkillsSection/SkillsSection.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { ResumeSection, SectionHeading, SectionSubHeading } from '../../common';
+import SkillsListItem from './SkillsListItem';
+import TechnologiesList from '../../TechnologiesList';
+
+const SkillsSection = () => (
+    <ResumeSection name="skills">
+        <div className="w-100">
+            <SectionHeading heading="Skills" />
+            <SectionSubHeading subHeading="Programming Languages &amp; Tools" />
+            <TechnologiesList />
+            <SectionSubHeading subHeading="Workflow" />
+            <ul className="fa-ul mb-0">
+                <SkillsListItem item="Mobile-First, Responsive Design" />
+                <SkillsListItem item="Cross Browser Testing &amp; Debugging" />
+                <SkillsListItem item="Cross Functional Teams" />
+                <SkillsListItem item="Agile Development &amp; Scrum" />
+            </ul>
+        </div>
+    </ResumeSection>
+);
+
+export default SkillsSection;
