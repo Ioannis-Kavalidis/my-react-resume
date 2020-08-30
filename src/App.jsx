ecdfe0c5167ab2a9e0df9873d6a57115aafa6ab3
paperjar <dreamtnem@gmail.com>
2020-08-30 18:16:56 +0200

    updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,32 @@
-import React from 'react';
+import React from "react";
 
-const App = () => <div className="app"></div>;
+const App = () => (
+  <div className="app">
+    <Navigation />
+    <Container fluid className="p-0">
+      {/* About Section */}
+      <AboutSection />
+      <SectionDivider />
 
+      {/* Experience Section */}
+      <ExperienceSection />
+      <SectionDivider />
+
+      {/* Education Section */}
+      <EducationSection />
+      <SectionDivider />
+
+      {/* Skills Section */}
+      <SkillsSection />
+      <SectionDivider />
+
+      {/* Interests Section */}
+      <InterestsSection />
+      <SectionDivider />
+
+      {/* Awards Section */}
+      <AwardsSection />
+    </Container>
+  </div>
+);
 export default App;
