134db433449676be99ce688f25d0cb794e4f4469
paperjar <dreamtnem@gmail.com>
2020-08-31 10:18:34 +0200

    updated the SkillsSection

--- src/components/Sections/SkillsSection/SkillsListItem.jsx
+++ src/components/Sections/SkillsSection/SkillsListItem.jsx
@@ -2,3 +2,18 @@ import React, { Fragment } from "react";
 import { string } from "prop-types";
 import { faCheck } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const SkillsListItem = ({ item }) => (
+  <Fragment>
+    <li>
+      <FontAwesomeIcon icon={faCheck} />
+      {item}
+    </li>
+  </Fragment>
+);
+
+SkillsListItem.propTypes = {
+  item: string.isRequired,
+};
+
+export default SkillsListItem;
--- src/components/Sections/SkillsSection/SkillsSection.jsx
+++ src/components/Sections/SkillsSection/SkillsSection.jsx
@@ -2,3 +2,22 @@ import React from "react";
 import { ResumeSection, SectionHeading, SectionSubHeading } from "../../common";
 import SkillsListItem from "./SkillsListItem";
 import TechnologiesList from "../../TechnologiesList";
+
+const SkillsSection = () => (
+  <ResumeSection name="skills">
+    <div className="w-100">
+      <SectionHeading heading="Skills" />
+      <SectionSubHeading subHeading="Programming Languages &amp; Tools" />
+      <TechnologiesList />
+      <SectionSubHeading subHeading="Workflow" />
+      <ul className="fa-ul mb-0">
+        <SkillsListItem item="Responsive Design" />
+        <SkillsListItem item="Debugging" />
+        <SkillsListItem item="Agile Development &amp; Scrum" />
+        <SkillsListItem item="Git &amp; Github " />
+      </ul>
+    </div>
+  </ResumeSection>
+);
+
+export default SkillsSection;
