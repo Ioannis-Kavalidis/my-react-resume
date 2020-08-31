728bb3b6c651f01889d0c259ee6997336fb343d9
paperjar <dreamtnem@gmail.com>
2020-08-31 10:24:51 +0200

    updated the TechnologiesSection

--- src/components/TechnologiesList/TechnologiesList.jsx
+++ src/components/TechnologiesList/TechnologiesList.jsx
@@ -1,16 +1,43 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-  faAngular,
+  faJsSquare,
   faCss3Alt,
-  faGrunt,
-  faGulp,
   faHtml5,
-  faLess,
   faNpm,
   faNodeJs,
   faReact,
   faSass,
-  faJsSquare,
   faWordpress,
 } from "@fortawesome/free-brands-svg-icons";
+
+const TechnologiesList = () => (
+  <ul className="list-inline technologies-list">
+    <li className="list-inline-item">
+      <FontAwesomeIcon icon={faHtml5} />
+    </li>
+    <li className="list-inline-item">
+      <FontAwesomeIcon icon={faCss3Alt} />
+    </li>
+    <li className="list-inline-item">
+      <FontAwesomeIcon icon={faJsSquare} />
+    </li>
+    <li className="list-inline-item">
+      <FontAwesomeIcon icon={faReact} />
+    </li>
+    <li className="list-inline-item">
+      <FontAwesomeIcon icon={faNodeJs} />
+    </li>
+    <li className="list-inline-item">
+      <FontAwesomeIcon icon={faSass} />
+    </li>
+    <li className="list-inline-item">
+      <FontAwesomeIcon icon={faWordpress} />
+    </li>
+    <li className="list-inline-item">
+      <FontAwesomeIcon icon={faNpm} />
+    </li>
+  </ul>
+);
+
+export default TechnologiesList;
