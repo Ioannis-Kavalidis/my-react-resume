94318111d647a24b75b082fd3630e1c27ad4ef58
paperjar <dreamtnem@gmail.com>
2020-09-01 11:40:34 +0200

    Updates

---
+++ precache-manifest.c7731cceb53c5f966d44e745afa478df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "480e9879fa77987e75e7",
+    "url": "my-react-resume/static/css/main.56ce1b79.chunk.css"
+  },
+  {
+    "revision": "480e9879fa77987e75e7",
+    "url": "my-react-resume/static/js/main.203a7f7b.chunk.js"
+  },
+  {
+    "revision": "3cb4bfe32021c0d85db0",
+    "url": "my-react-resume/static/js/runtime~main.db16fb92.js"
+  },
+  {
+    "revision": "22751990a3ecb98d8aa7",
+    "url": "my-react-resume/static/js/2.3b0d0bfc.chunk.js"
+  },
+  {
+    "revision": "19285fb25fd2806fe4c5dc0aec558a8c",
+    "url": "my-react-resume/static/media/profile.19285fb2.jpg"
+  },
+  {
+    "revision": "0ed4869a6bc61dafcb6ae9d4438bea1c",
+    "url": "my-react-resume/index.html"
+  }
+];
