63ec3cacfd989aaff5db435cb2af9f4a421d6740
paperjar <dreamtnem@gmail.com>
2020-09-07 20:56:00 +0200

    small additions

--- src/components/Sections/InterestsSection/InterestsSection.jsx
+++ src/components/Sections/InterestsSection/InterestsSection.jsx
@@ -1,25 +1,23 @@
-import React from "react";
-import { ResumeSection, SectionHeading } from "../../common";
+import React from 'react';
+import { ResumeSection, SectionHeading } from '../../common';
 
 const InterestsSection = () => (
-  <ResumeSection name="interests">
-    <div className="w-100 text-dark">
-      <SectionHeading heading="Interests" />
-      <p>
-        Apart from being a web developer, I enjoy reading books and articles on
-        self improvement, business development and entrepreneurship, traveling
-        solo and with friends. Participate in various social, professional and
-        cultural events such as: “EXPO Athens”, TEDx Athens, Meet Up, Startup
-        Blink, Open Coffee etc.
-      </p>
-      <p className="mb-0">
-        Favourite book: “The seven habits of highly effective people” by Stephen
-        R. Covey <br />
-        Current reading: “Abundance: The future is better than you think” by
-        Peter Diamandis
-      </p>
-    </div>
-  </ResumeSection>
+    <ResumeSection name="interests">
+        <div className="w-100 text-dark">
+            <SectionHeading heading="Interests" />
+            <p>
+                Apart from being a web developer, I enjoy reading books and articles on self
+                improvement, business development and entrepreneurship, traveling solo and with
+                friends. Participate in various social, professional and cultural events such as:
+                EXPO Athens, TEDx Athens, Meet Up, Startup Blink, Open Coffee etc.
+            </p>
+            <p className="mb-0">
+                Favourite book: “The seven habits of highly effective people” by Stephen R. Covey{' '}
+                <br />
+                Current reading: “Abundance: The future is better than you think” by Peter Diamandis
+            </p>
+        </div>
+    </ResumeSection>
 );
 
 export default InterestsSection;
